654c43e0fbe1c006f4cd04144f1dd9df7169204c
namwoody <deepsarora262@gmail.com>
2016-03-27 21:36:22 -0400

    add header scss file & disbale ssl for production

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,23 @@ Rails.application.routes.draw do
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
   #
   # We ask that you don't use the :as option here, as Spree relies on it being the default of "spree"
+ 
+
+
+
+
   mount Spree::Core::Engine, :at => '/'
+  
+
+
           resources :posts
+
+
+  
+
+
+
+
 
 
 end
