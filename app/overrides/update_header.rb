5f0980c27788cd354556a6d411d451f5d6d0bcb2
namwoody <deepsarora262@gmail.com>
2016-03-29 00:46:03 -0400

    edit a devise file

---
+++ app/overrides/update_header.rb
@@ -0,0 +1,14 @@
+Deface::Override.new{virtual_path:  "spree/shared/_header",
+	    name: "add some text",
+	    insert_after: ".top-bar",
+	    text: "
+         <p>
+   
+         </p>
+        
+
+
+	    "
+
+
+}
